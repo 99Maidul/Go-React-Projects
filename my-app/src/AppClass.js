8407715128fd316314aa2b4f27d35f716f0f221d
CoderNoob2021 <islam.maidul567@gmail.com>
2023-02-21 11:36:20 +0900

    Bootstrap added

--- my-app/src/AppClass.js
+++ my-app/src/AppClass.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./AppClass.css"
 
 export default class AppClass extends Component {
     render() {
         return (
             <div>
-                <h1>Class Component</h1>
+                <h1 className="h1-red">Class Component</h1>
             </div>
         )
     }
---
+++ my-app/src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function HelloWorld() {
+    return (
+        <Fragment>
+            <h1>Hello World</h1>
+            <p>This is a paragraph</p>
+        </Fragment>
+    )
+}
+export default HelloWorld;
