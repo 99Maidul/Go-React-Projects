63355a1e7e0075d16ca14e2c63e819549eae0aa3
CoderNoob2021 <islam.maidul567@gmail.com>
2023-02-21 10:50:18 +0900

    Initial commit

---
+++ my-app/src/AppClass.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Class Component</h1>
+            </div>
+        )
+    }
+}
