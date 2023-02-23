5bd98fc35e224283fb71ce0f27f1fa50dc0e7aaa
CoderNoob2021 <islam.maidul567@gmail.com>
2023-02-24 01:41:52 +0900

    Class state

--- my-app/src/AppClass.js
+++ my-app/src/AppClass.js
@@ -1,12 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./AppClass.css"
 
 export default class AppClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isTrue: true
+        }
+    }
+
+    toggleTrue = () => {
+        if (this.state.isTrue) {
+            this.setState({
+                isTrue: false,
+            });
+            return
+        }
+        this.setState({
+            isTrue: true,
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1 className="h1-red">{this.props.msg}</h1>
-            </div>
+            <Fragment>
+                <hr />
+                <h1>{this.props.msg}</h1>
+                <hr />
+                {this.state.isTrue &&
+                    <Fragment>
+                        <p>The current value of isTrue is true</p>
+                    </Fragment>
+                }
+                <hr />
+                {this.stateisTrue
+                    ? <p>The current value of isTrue is true</p>
+                    : <p>The current value of isTrue is false</p>
+                }
+                <a href="#!" className="btn btn-outline-secondary" onClick={this.toggleTrue}>Toggle isTrue</a>
+
+            </Fragment>
         )
     }
 }
