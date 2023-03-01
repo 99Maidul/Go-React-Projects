22380bca3050b03d681cc8d3f62cc21127b586a7
CoderNoob2021 <islam.maidul567@gmail.com>
2023-03-01 09:56:40 +0900

    React basics

--- my-app/src/Input.js
+++ my-app/src/Input.js
@@ -1,15 +1,19 @@
-const Input = (props) => {
-    return (
+import { forwardRef } from "react";
+
+const Input = forwardRef((props, ref) => {
+    return(
         <div className="mb-3">
-            <label htmlFor="{props.name}" className="form-label">{props.title}</label>
+            <label htmlFor={props.name} className="form-label">{props.title}</label>
             <input
                 type={props.type}
                 className={props.className}
                 id={props.name}
-                autoComplete={props.AutoComplete}
+                ref={ref}
+                autoComplete={props.autoComplete}
                 onChange={props.onChange}
             />
         </div>
     )
-}
+});
+
 export default Input;
