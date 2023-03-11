945b8a58cdf7b99811e29fba822993128bffa709
CoderNoob2021 <islam.maidul567@gmail.com>
2023-03-12 01:49:24 +0900

    All folders

--- golang-jwt-project/routes/userRouter.go
+++ golang-jwt-project/routes/userRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	controller "golang-jwt-project/controllers"
+	"golang-jwt-project/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middlewares.Authenticate())
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+}
